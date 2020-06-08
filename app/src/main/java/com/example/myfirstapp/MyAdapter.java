75aa94692fc65c04508b0e8e7812721d443a7b7d
geraldine <hypatia.of.alexandria55@gmail.com>
2020-06-08 19:34:49 -0400

    fixed layout of recyclerview

--- app/src/main/java/com/example/myfirstapp/MyAdapter.java
+++ app/src/main/java/com/example/myfirstapp/MyAdapter.java
@@ -2,22 +2,26 @@ package com.example.myfirstapp;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private String[] mDataset;
 
     // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class MyViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
+        // all components you might use should be declared here, linked as well
+        public LinearLayout cellView;
+        public TextView cellText;
+        // ATTACH ... im not sure if should be only outercontainer here or also textView
+        public MyViewHolder(LinearLayout v) {
             super(v);
-            textView = v;
+            cellText = (TextView) v.findViewById(R.id.cellText);
+            cellView = v;
         }
     }
 
@@ -31,7 +35,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
                                                      int viewType) {
         // create a new view (cell content)
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.camera_roll_cell, parent, false);
         MyViewHolder vh = new MyViewHolder(v);
         return vh;
@@ -40,9 +44,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
+        // set photo (text rn)
+        holder.cellText.setText(mDataset[position]);
 
     }
 
